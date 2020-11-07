3a898d8844483f7b60ec5729915284c1884705a6
Ferzik857 <Aleksandar.Pastuhov@yandex.ru>
2020-11-07 21:53:45 +0200

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className="header">
+    <img src="https://www.clipartmax.com/png/full/220-2205004_gallery-of-free-logo-maker-design-with-jeta-software-free-3d-logo.png" />
+   </header>
+}
+
+export default  Header;
