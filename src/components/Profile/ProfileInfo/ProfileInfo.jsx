6e905f6419bf8d7f1e1ddc576ac448be208829c5
Ferzik857 <Aleksandar.Pastuhov@yandex.ru>
2021-01-13 14:49:13 +0200

    hook

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 
 import Preloader from '../../common/Preloader/Preloader';
 import  a from './ProfileInfo.module.css'
-import ProfileStatus from "./ProfileStatus" 
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 const ProfileInfo = (props) => {
   if(!props.profile){
     return <Preloader />
@@ -13,7 +13,7 @@ const ProfileInfo = (props) => {
 </div>*/}
 <div className={a.descriptionBlock}>
   <img src={props.profile.photos.large}/>
-  <ProfileStatus status = {props.status} updateStatus={props.updateStatus}/>
+  <ProfileStatusWithHooks status = {props.status} updateStatus={props.updateStatus}/>
 </div>
 </div>)
 }
