cde668ed1c0b96cad2bc2edf7eaea5f20aab125d
Ferzik857 <Aleksandar.Pastuhov@yandex.ru>
2021-01-28 21:07:47 +0200

    WebSocket

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import {Redirect, Route, Switch, withRouter} from 'react-router-dom';
+import {Link, NavLink, Redirect, Route, Switch, withRouter} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import Friends from './components/Friends/Friends';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-
 import ProfileCountainer from './components/Profile/ProfileCountainer';
-import HeaderContainer from './components/Heder/HeaderContainer';
 import Login from './components/login/login';
 import { Component } from 'react';
 import { connect } from 'react-redux';
@@ -16,6 +13,19 @@ import { compose } from 'redux';
 import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
 import { UsersPage } from './components/Users/UsersContainer';
+import "antd/dist/antd.css"
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Header } from './components/Heder/Header';
+import ChatPage from './pages/Chat/ChatPage';
+
+
+const { SubMenu } = Menu;
+const {  Content, Footer, Sider } = Layout;
+
+
+
+
 
 
 class App extends Component {
@@ -25,12 +35,45 @@ render(){
   if (!this.props.initialized){
   return <Preloader />}
   return (
-   
-    <div className="app-warpper">
-<HeaderContainer/>
-<Navbar/>
-<div className="app-warpper-content">
-  <Switch>
+    <Layout>
+    <Header />
+    
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+        <Sider className="site-layout-background" width={200}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+           // defaultOpenKeys={['sub1']}
+            style={{ height: '100%' }}
+          >
+            <SubMenu key="sub1" icon={<UserOutlined />} title="My Profile">
+              <Menu.Item key="1"><Link to="/Profile" >Profile</Link></Menu.Item>
+              <Menu.Item key="2"><Link to="/Dialogs">Messages</Link></Menu.Item>
+              <Menu.Item key="3"><Link to="/News" >News</Link></Menu.Item>
+              <Menu.Item key="4"><Link to="/Music">Music</Link></Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+              <Menu.Item key="5"><Link to="/Settings">Settings</Link></Menu.Item>
+              <Menu.Item key="6"><Link to="/Friends">Friends</Link></Menu.Item>
+              <Menu.Item key="7"><Link to="/Users" >Users</Link></Menu.Item>
+              <Menu.Item key="8"><Link to="/Chat" >Chat</Link></Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+        <Switch>
   <Route exact path="/" render= {() => <Redirect to={'/profile'}/>}/>
   <Route path="/login" render={() => <Login />}/>
   <Route path="/Profile/:userId?" render={() => <ProfileCountainer />}/>
@@ -40,10 +83,12 @@ render(){
   <Route path="/Settings" render={Settings}/>
   <Route path="/Friends" render={Friends}/>
   <Route path="/users" render={() => <UsersPage pageTitle={"title"}/>}/>
- </Switch>
-    </div>
-    </div>
-   
+  <Route path="/Chat" render={ChatPage}/>
+ </Switch></Content>
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ferzik@Prodakchen</Footer>
+  </Layout>
   );
 }
 }
--- src/components/Heder/Header.jsx
+++ src/components/Heder/Header.jsx
@@ -1,8 +1,60 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import  b from './Header.module.css'
+import { Button, Col, Row } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import Avatar from 'antd/lib/avatar/avatar';
+import { UserOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsAuth, selectCurrentUserLogin } from '../../redux/auth-selectors';
+import { logout } from '../../redux/auth-reduser';
 
-const Header = (props) => {
-    return <header className={b.header}>
+export const Header = (props) => {
+
+const isAuth = useSelector(selectIsAuth)
+const login = useSelector(selectCurrentUserLogin)
+const dispatch = useDispatch()
+const logoutCallback = ()=> {
+    dispatch(logout())
+}
+
+    const { Header } = Layout;
+    return  <Header className="header">
+    <div className="logo" />
+    <Row>
+      <Col span={18}>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      <Menu.Item key="1"><Link to="/Users" >Developers</Link></Menu.Item>
+      </Menu> 
+      </Col>
+      
+      {isAuth 
+      ?<> <Col span={1}>
+          <Avatar alt={login || ""} style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+      </Col>
+      <Col span={5}>
+       <Button onClick={logoutCallback}>log out</Button> 
+      </Col>
+      </>
+      :<Col span={6}>
+      <Button>
+      <Link to={"/login"}>Login</Link>
+      </Button>
+      </Col>}
+    </Row>
+   
+
+  </Header>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   /* <header className={b.header}>
     <img src="https://www.clipartmax.com/png/full/220-2205004_gallery-of-free-logo-maker-design-with-jeta-software-free-3d-logo.png" />
   <div className ={b.loginBlock}>
       {props.isAuth 
@@ -10,7 +62,7 @@ const Header = (props) => {
       :<NavLink to={"/login"}>Login</NavLink> }
       
   </div>
-   </header>
+   </header>*/
 }
 
-export default  Header;
+
